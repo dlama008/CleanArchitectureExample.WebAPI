17d021700f3a36bf9ce207df7a4f9708eefdc8c9
Lauri Marttinen <dlama008@edu.xamk.fi>
2024-03-28 10:29:04 +0200

    Add project files.

---
+++ Controllers/GreetingController.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitectureExample.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureExample.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class GreetingController : Controller
+    {
+        private readonly IGreetingService _greetingService;
+        
+        public GreetingController(IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<string> Get(string name)
+        {
+            var greeting = _greetingService.Greet(name);
+            return Ok(greeting);
+        }
+    }
+}
